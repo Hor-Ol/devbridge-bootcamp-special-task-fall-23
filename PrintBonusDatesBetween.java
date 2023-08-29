fd8ffe88c1e56d94247a9cd9d026c90cf1e07cc3
Hor-Ol <oleksandrhorobets16@gmail.com>
2023-08-29 19:18:51 +0200

    Updated version of method with localDate

--- PrintBonusDatesBetween.java
+++ PrintBonusDatesBetween.java
@@ -1,5 +1,6 @@
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class PrintBonusDatesBetween {
 
@@ -8,27 +9,42 @@ public class PrintBonusDatesBetween {
   }
 
   static void PrintBonusDatesBetween(int fromYear, int toYear) {
-    // Date formating pattern:
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-    // Setting starting date:
-    Calendar fromYearDate = Calendar.getInstance();
-    fromYearDate.set(fromYear, 0, 1);
+    // --- INITIAL INPUT СHECKS:
+    // Specifying allowed years range:
+    if ((fromYear < 1000 || fromYear > 9999) || (toYear < 1000 || toYear > 9999)) {
+      System.out
+          .println("\n NOT ALLOWED: Please note that years cannot be smaller than 1000 and bigger than 9999 \n");
+      return;
+    }
 
-    // Setting ending date:
-    Calendar toYearDate = Calendar.getInstance();
-    toYearDate.set(toYear, 11, 31);
+    // Sense-checking whether starting year is not bigger than ending year:
+    if (toYear < fromYear) {
+      System.out.println("\n NOT ALLOWED: Please note that starting year cannot be bigger than ending year \n");
+      return;
+    }
 
+    // --- DATA PREPARATION & FORMATTING:
+    // Date formating pattern for dates:
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    // Setting starting date by adding first day and month of the year:
+    LocalDate fromYearDate = LocalDate.of(fromYear, Month.JANUARY, 1);
+
+    // Setting ending date by adding last day and month of the year:
+    LocalDate toYearDate = LocalDate.of(toYear, Month.DECEMBER, 31);
+
+    // --- CHECKING THE DATES AND OUTPUTING RESULTS:
     // Palindrome dates checking mechanism:
-    for (Calendar iteratingDate = fromYearDate; (iteratingDate.before(toYearDate)
-        || iteratingDate.equals(toYearDate)); iteratingDate.add(Calendar.DATE, 1)) {
-      String dateString = sdf.format(iteratingDate.getTime()).toString();
+    for (LocalDate iteratingDate = fromYearDate; (iteratingDate.isBefore(toYearDate)
+        || iteratingDate.isEqual(toYearDate)); iteratingDate = iteratingDate.plusDays(1)) {
+      String dateString = iteratingDate.format(formatter);
       String formatedDateString = dateString.replace("-", "");
 
       // (Re)Setting the amount of matches:
       int matches = 0;
 
-      // The loop below compares the first number of the date equals the last number of the date, second equals second form the end and so on. If the is a match, then the "matches" variable is incremented: 
+      // The loop below compares the first digit of the date equals the last digit of the date, second equals second form the end and so on. If there is a match, then the "matches" variable is incremented: 
       for (int i = 0, j = formatedDateString.length() - 1; i < formatedDateString.length() / 2; i++, j--) {
 
         if (formatedDateString.charAt(i) == formatedDateString.charAt(j)) {
@@ -37,7 +53,7 @@ public class PrintBonusDatesBetween {
 
       }
 
-      // If there are 4 matches, it means that the date  can be effectivelly reversed and still show the same date. In such case we're printing the date:
+      // If there are 4 matches, it means that the date can be effectivelly reversed and still show the same date. In such case we're printing the date:
       if (matches == 4)
         System.out.println(dateString);
     }
